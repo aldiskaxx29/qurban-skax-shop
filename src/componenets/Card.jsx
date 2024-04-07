253d554c28e364397d2a395057660a1b15577d0c
aldiskax_29 <89407699+aldiskaxx29@users.noreply.github.com>
2024-04-07 12:59:47 +0700

    add carousel

--- src/componenets/Card.jsx
+++ src/componenets/Card.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import Slider from "react-slick";
 
 const Card = ({left = false, title, menu}) => {
   console.log(left,title, menu);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false
+  };
+
   return (
     <div>
       <div className='flex flex-row justify-between'>
         <p className='text-2xl font-bold'>{title}</p>
         <ul className='flex space-x-3'>
-          {menu.map((item) => (
-            <li>{item.name}</li>
+          {menu.map((item, index) => (
+            <li key={index}>{item.name}</li>
           ))}
         </ul>
       </div>
@@ -19,17 +29,26 @@ const Card = ({left = false, title, menu}) => {
                 <div className='col-span-2'>
                   <div className='h-[300px] bg-slate-300 rounded-md'></div>
                 </div>
-                <div className='col-span-1'>
-                  <div className='h-[300px] bg-slate-300 rounded-md'></div>
-                </div>
-                <div className='col-span-1'>
-                  <div className='h-[300px] bg-slate-300 rounded-md'></div>
-                </div>
-                <div className='col-span-1'>
-                  <div className='h-[300px] bg-slate-300 rounded-md'></div>
-                </div>
-                <div className='col-span-1'>
-                  <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                <div className='col-span-4'>
+                  <div className="slider-container" >
+                    <Slider {...settings} >
+                      <div className='col-span-1 px-2' style={{ padding: '0 10px' }}>
+                        <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                      </div>
+                      <div className='col-span-1 px-2' style={{ padding: '0 10px' }}>
+                        <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                      </div>
+                      <div className='col-span-1 px-2' style={{ padding: '0 10px' }}>
+                        <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                      </div>
+                      <div className='col-span-1 px-2' style={{ padding: '0 10px' }}>
+                        <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                      </div>
+                      <div className='col-span-1 px-2' style={{ padding: '0 10px' }}>
+                        <div className='h-[300px] bg-slate-300 rounded-md'></div>
+                      </div>
+                    </Slider>
+                  </div>
                 </div>
               </div>
             </div>
