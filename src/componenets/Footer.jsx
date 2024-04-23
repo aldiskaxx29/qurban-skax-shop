7ab93b2b42fef367646c4098bfd3106cb2d5c399
aldiskax_29 <89407699+aldiskaxx29@users.noreply.github.com>
2024-04-23 11:33:36 +0700

    update footer

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -39,17 +39,17 @@ const Footer = () => {
       </div>
 
       <div className='grid grid-cols-3 gap-3 py-3 border-t-green-400'>
-        <div className='col-span-1'>
+        <div className='col-span-3 md:col-span-1 text-center md:text-left'>
           <div className='p-4 rounded-md '>
-            <p>©2024 QurbanAldiSkax</p>
+            <p className='text-[12px] md:text-[14px]'>©2024 QurbanAldiSkax</p>
           </div>
         </div>
-        <div className='col-span-1'>
+        <div className='col-span-1 hidden md:block'>
           <div className='p-4 rounded-md  text-center'>
             <p>+62 877-0153-6399</p>
           </div>
         </div>
-        <div className='col-span-1'>
+        <div className='col-span-1 hidden md:block'>
           <div className='p-4 rounded-md text-right'>
             <p>Follow Us</p>
             <ul>
